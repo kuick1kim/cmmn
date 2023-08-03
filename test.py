a0dd5ccf8864431328edf1861bbf8baf5d373b6f
KimMinSang <kuick1@gmail.com>
2023-08-03 18:30:08 +0900

    change1

--- test.py
+++ test.py
@@ -6,27 +6,3 @@ import common_git
 
 common_git.update('change1')
 
-# os.system('git config --list')
-# os.system('git add README.md')
-# os.system('git add .')
-
-
-# os.system('git commit -m "first commit"')
-# os.system('git remote add origin https://github.com/kuick1kim/common_git.git')
-# os.system('git push origin main')
-
-
-
-# 오리진이 문제가 있을때 지우기
-# error: remote origin already exists.  
-# os.system('git remote remove origin')
-
-# 문제2 
-# error: failed to push some refs to 'https://github.com/kuick1kim/common_git.git'
-
-
-# 문제3
-# fatal: a branch named 'master' already exists
-# 삭제해줘야 한다. 
-# fatal: a branch named 'master' already exists
-# git branch -d master    
