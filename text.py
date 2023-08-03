53c1593afe9ad09b60deda7a9a5b769afdcdf329
KimMinSang <kuick1@gmail.com>
2023-08-03 18:09:29 +0900

    first commit

---
+++ text.py
@@ -0,0 +1,16 @@
+
+
+
+
+import os
+
+# os.system('git config --list')
+# os.system('git add README.md')
+os.system('git add .')
+
+
+os.system('git commit -m "first commit"')
+os.system('git remote add origin https://github.com/kuick1kim/common_git.git')
+os.system('git push -u origin main')
+
+
